b368b84d2c08e0a25f2c577dd9d82752336627e8
Roman <Romanklacko5138@gmail.com>
2021-03-27 22:44:45 +0300

    initial

---
+++ less_1.py
@@ -0,0 +1,19 @@
+# print(type(int))
+# print(type(type))
+# print(type.__module__)
+# print(str)
+# help(type)
+
+
+# print(type.__bases__)
+# print(str.__bases__)
+# print(object.__class__)
+# my_string = 'some string'
+# print(type(my_string))
+# my_string2 = str('some string')
+# print(type(my_string2))
+# print(my_string == my_string2)
+# print(my_string is my_string2)
+# my_string2 = 'some string'
+# print(my_string == my_string2)
+# print(my_string is my_string2)
